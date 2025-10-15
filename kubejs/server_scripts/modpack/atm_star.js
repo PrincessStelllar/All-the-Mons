44110a121ab76deef7ec490576f4f108ebedfbc7
Satherov <satherov@users.noreply.github.com>
2025-10-15 23:04:16 +0200

    Fix Create ATM Star

--- kubejs/server_scripts/modpack/atm_star.js
+++ kubejs/server_scripts/modpack/atm_star.js
@@ -1,60 +1,127 @@
-// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
-ServerEvents.recipes(allthemods => {
-  allthemods.custom({
-    type: "create:mechanical_crafting",
-    pattern: [
-      '    A    ',
-      '   AJA   ',
-      'AAAJFJAAA',
-      'AJJCDEJJA',
-      ' AMBIHMA ',
-      '  AKGLA  ',
-      ' AJJAJJA ',
-      'AJJA AJJA',
-      'AAA   AAA'
-    ],
-    key: {
-      A: Ingredient.of('allthemodium:unobtainium_allthemodium_alloy_block').toJson(),
-      B: Ingredient.of('allthetweaks:nexium_emitter').toJson(),
-      C: Ingredient.of('allthetweaks:dragon_soul').toJson(),
-      D: Ingredient.of('allthetweaks:withers_compass').toJson(),
-      E: Ingredient.of('allthetweaks:pulsating_black_hole').toJson(),
-      F: Ingredient.of('allthetweaks:oblivion_shard').toJson(),
-      G: Ingredient.of('allthetweaks:improbable_probability_device').toJson(),
-      H: Ingredient.of('allthetweaks:dimensional_seed').toJson(),
-      I: Ingredient.of('allthetweaks:patrick_star').toJson(),
-      J: Ingredient.of('allthecompressed:nether_star_block_3x').toJson(),
-      K: Ingredient.of('allthetweaks:philosophers_fuel').toJson(),
-      L: Ingredient.of('mysticalagradditions:creative_essence').toJson(),
-      M: Item.of('allthemodium:unobtainium_vibranium_alloy_block', "{HideFlags:1,display:{Name:'[{\"text\":\"Awakened Unobtainium-Vibranium Alloy Block\",\"italic\":false}]'}}").enchant('unbreaking', 1).strongNBT().toJson()
-    },
-    result: Ingredient.of('allthetweaks:atm_star').toJson(),
-    acceptMirrored: false
-  }).id('allthemods:allthetweaks/atm_star')
-  
-  allthemods.custom({
-    type: "create:mechanical_crafting",
-    pattern: [
-      '    S    ',
-      '   SSS   ',
-      'SSSSSSSSS',
-      'SSSSSSSSS',
-      ' SSSPSSS ',
-      '  SSSSS  ',
-      ' SSSSSSS ',
-      'SSSS SSSS',
-      'SSS   SSS'
-    ],
-    key: {
-      S: Ingredient.of('allthetweaks:atm_star_shard').toJson(),
-      P: Ingredient.of('allthetweaks:patrick_star').toJson(),
-    },
-    result: Ingredient.of('allthetweaks:atm_star').toJson(),
-    acceptMirrored: false
-  }).id('allthemods:allthetweaks/atm_shard_star')
-})
+ServerEvents.recipes((allthemods) => {
+  //ATM Star - Create
+  allthemods
+    .custom({
+      type: "create:mechanical_crafting",
+      accept_mirrored: false,
+      category: "misc",
+      key: {
+        A: {
+          item: "allthemodium:unobtainium_allthemodium_alloy_block",
+        },
+        B: {
+          item: "allthetweaks:nexium_emitter",
+        },
+        C: {
+          item: "allthetweaks:dragon_soul",
+        },
+        D: {
+          item: "allthetweaks:withers_compass",
+        },
+        E: {
+          item: "allthetweaks:pulsating_black_hole",
+        },
+        F: {
+          item: "allthetweaks:oblivion_shard",
+        },
+        G: {
+          item: "allthetweaks:improbable_probability_device",
+        },
+        H: {
+          item: "allthetweaks:dimensional_seed",
+        },
+        I: {
+          item: "allthetweaks:patrick_star",
+        },
+        J: {
+          item: "allthecompressed:nether_star_block_3x",
+        },
+        K: {
+          item: "allthetweaks:philosophers_fuel",
+        },
+        L: {
+          item: "mysticalagradditions:creative_essence",
+        },
+        M: {
+          item: "cobblemon:master_ball",
+        },
+      },
+      pattern: [
+        "    A    ",
+        "   AJA   ",
+        "AAAJFJAAA",
+        "AJJCDEJJA",
+        " AMBIHMA ",
+        "  AKGLA  ",
+        " AJJAJJA ",
+        "AJJA AJJA",
+        "AAA   AAA",
+      ],
+      result: {
+        count: 1,
+        id: "allthetweaks:atm_star",
+      },
+      show_notification: false,
+    })
+    .id("allthetweaks:allthetweaks/atm_star");
 
-// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 9.
+  //Patric Star - Create
+  allthemods
+    .custom({
+      type: "create:mechanical_crafting",
+      accept_mirrored: false,
+      category: "misc",
+      key: {
+        A: {
+          item: "minecraft:magenta_concrete",
+        },
+        B: {
+          item: "minecraft:pink_concrete",
+        },
+        C: {
+          item: "minecraft:pink_concrete_powder",
+        },
+        D: {
+          item: "minecraft:green_concrete",
+        },
+        E: {
+          item: "minecraft:green_concrete_powder",
+        },
+        F: {
+          item: "minecraft:lime_concrete",
+        },
+        F: {
+          item: "minecraft:lime_concrete",
+        },
+        F: {
+          item: "minecraft:lime_concrete",
+        },
+        G: {
+          item: "minecraft:magenta_concrete_powder",
+        },
+      },
+      pattern: [
+        "    B    ",
+        "   BCA   ",
+        "BBBGCABBB",
+        "ACGCGGGCA",
+        " AAGGGBA ",
+        "  EDDDE  ",
+        " EFEDEFE ",
+        "BEDD DDEB",
+        "AAA   AAA",
+      ],
+      result: {
+        count: 1,
+        id: "allthetweaks:patrick_star",
+      },
+      show_notification: false,
+    })
+    .id("allthetweaks:allthetweaks/patrick_star");
+});
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
