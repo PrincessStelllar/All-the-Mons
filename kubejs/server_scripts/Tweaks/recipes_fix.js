f499bea1d23960aef3af0d4d7b270faa6223de30
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2026-01-16 16:50:13 -0300

    many changes:
    - set default loading FML to dark theme and guiScale 2
    - add our server to server list
    - update menu brackground and loading background
    - create "ATM Series" on Radical Cobblemon Trainers
      requires "bdsp" to be defeated
    - add trainers:
      - CesarZorak
      - _Rosary_
      - Notch
    - disable kubejs webserver to prevent high CPU usage
    - add Elite Gym structure
    - add post processor to make create contraptions work in structures

--- kubejs/server_scripts/Tweaks/recipes_fix.js
+++ kubejs/server_scripts/Tweaks/recipes_fix.js
@@ -224,12 +224,17 @@ KubeJSTweaks.beforeRecipes(event => {
       entry.replaceValueAtKey("ingredients", "fluid_tag", "c:chocolates", "c:chocolate")
     })
 	
-    event.getEntry("mekmm:compat/mysticalagradditions/planting/awakened_draconium")
+  event.getEntry("mekmm:compat/mysticalagradditions/planting/awakened_draconium")
     .forEach(entry => {
-	    entry.fixItemAtKey("main_output")
-	    let ci = entry.json().get("chemical_input")
+      entry.fixItemAtKey("main_output")
+      let ci = entry.json().get("chemical_input")
       ci.add("chemical", ci.remove("gas"))
-	  })
+    })
+
+  event.getEntry("botanypots:allthemodium/crop/ancient_soulberries")
+    .forEach(entry => {
+      entry.addConditionsFromKey("input")
+    })
 
   console.log(`Fixing recipes took ${timer.stop().elapsed("milliseconds")} ms...`)
 })
