4b59ff9a23eb9f90f7b2d06e223e9e62b491dd5b
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2026-01-20 13:36:59 -0300

    many fixes

--- kubejs/server_scripts/mods/Cobblemon/Recipes.js
+++ kubejs/server_scripts/mods/Cobblemon/Recipes.js
@@ -3,12 +3,14 @@
 
 ServerEvents.recipes(allthemods => {
     allthemods.remove({type: "minecraft:crafting_shaped", output: "#cobblemon:poke_balls"})
-    allthemods.shaped('cobblemon:master_ball', ['EBE', 'BUB', 'NNN'], {
-        U: 'allthemons:unobtainium_ball',
-        B: 'allthemons:soul_lava_ball',
-        E: 'allthemons:vibranium_ball',
-        N: 'allthemons:allthemodium_ball'
-    }).id('cobblemon:master_ball')
+    // allthemods.shaped('cobblemon:master_ball', ['EBE', 'BUB', 'NNN'], {
+    //     U: 'allthemons:unobtainium_ball',
+    //     B: 'allthemons:soul_lava_ball',
+    //     E: 'allthemons:vibranium_ball',
+    //     N: 'allthemons:allthemodium_ball'
+    // }).id('cobblemon:master_ball')
+    allthemods.remove("createmonballsoverhaul:sequenced_assembly/balls/master_ball")
+    allthemods.remove("createmonballsoverhaul:sequenced_assembly/balls/ancient_origin_ball")
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
