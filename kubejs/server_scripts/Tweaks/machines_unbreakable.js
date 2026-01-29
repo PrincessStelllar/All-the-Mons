7fb46cf6ec546034c50e48cdf691630b1e086973
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2026-01-29 14:41:11 -0300

    fixes

--- kubejs/server_scripts/Tweaks/machines_unbreakable.js
+++ kubejs/server_scripts/Tweaks/machines_unbreakable.js
@@ -1,9 +1,11 @@
 let $Tags = Java.loadClass("dev.latvian.mods.kubejs.util.Tags")
-let $HealingMachineBlock = Java.loadClass("com.cobblemon.mod.common.block.HealingMachineBlock")
 
 let machinesTag = $Tags.block("cobblemon:machines")
 NativeEvents.onEvent("net.neoforged.neoforge.event.entity.player.PlayerEvent$BreakSpeed", event => {
   if (event.state["is(net.minecraft.tags.TagKey)"](machinesTag)) {
-	  event.state.getOptionalValue($HealingMachineBlock.Companion.NATURAL).ifPresent(value => {if (value) event.setCanceled(true)})
+	  let natural = event.state.block?.Companion?.NATURAL
+	  if (natural != null) {
+	    event.state.getOptionalValue(natural).ifPresent(value => {if (value) event.setCanceled(true)})		  
+	  }
   }
 })
