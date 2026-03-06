6919b157402d5b1ffa5493361311948caf6a11d5
Jais Djurhuus-Kempel <jdk@jdkdigital.cy>
2026-03-06 20:11:15 +0200

    Complete list of non-naturally spawning mons

--- kubejs/server_scripts/Tweaks/disable_mons.js
+++ kubejs/server_scripts/Tweaks/disable_mons.js
@@ -1,9 +1,20 @@
 ServerEvents.generateData('last', allthemons => {
 
   let mons = [
-      "blacephalon", "buzzwole", "celebi", "chienpao", "cobalion", "fluttermane", "genesect",
-      "ironcrown", "jirachi", "kartana", "magearna", "marshadow", "meltan", "nihilego", "ogerpon",
-      "pecharunt", "poipole", "screamtail", "shaymin", "stakataka", "terrakion", "xurkitree", "kubfu"
+      "articuno", "mew", "mewtwo", "moltres", "zapdos", "celebi", "entei", "hooh", "lugia", "raikou", "suicune",
+      "deoxys", "groudon", "jirachi", "kyogre", "latias", "latios", "rayquaza", "regice", "regirock", "registeel",
+      "arceus", "azelf", "cresselia", "darkrai", "dialga", "giratina", "heatran", "manaphy", "mesprit", "palkia",
+      "phione", "regigigas", "shaymin", "uxie", "cobalion", "genesect", "keldeo", "kyurem", "landorus", "meloetta",
+      "reshiram", "terrakion", "thundurus", "tornadus", "victini", "virizion", "zekrom", "diancie", "hoopa",
+      "volcanion", "xerneas", "yveltal", "zygarde", "blacephalon", "buzzwole", "celesteela", "cosmoem", "cosmog",
+      "guzzlord", "kartana", "lunala", "magearna", "marshadow", "naganadel", "necrozma", "nihilego", "pheromosa",
+      "poipole", "silvally", "solgaleo", "stakataka", "tapubulu", "tapufini", "tapukoko", "tapulele", "typenull",
+      "xurkitree", "zeraora", "melmetal", "meltan", "calyrex", "eternatus", "glastrier", "kubfu", "regidrago",
+      "regieleki", "spectrier", "urshifu", "zacian", "zamazenta", "zarude", "enamorus", "brutebonnet", "chienpao",
+      "chiyu", "fezandipiti", "fluttermane", "gougingfire", "greattusk", "ironboulder", "ironbundle", "ironcrown",
+      "ironhands", "ironjugulis", "ironleaves", "ironmoth", "ironthorns", "irontreads", "ironvaliant", "koraidon",
+      "miraidon", "munkidori", "ogerpon", "okidogi", "pecharunt", "ragingbolt", "roaringmoon", "sandyshocks",
+      "screamtail", "slitherwing", "terapagos", "tinglu", "walkingwake", "wochien"
   ]
 
   mons.forEach((mon) => {
